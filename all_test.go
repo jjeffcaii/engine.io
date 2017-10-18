f852c394f713b86d988fe8b22ac275dbf6334793
jjeffcaii <jjeffcaii@outlook.com>
2017-10-18 18:16:03 +0800

    这个人很懒, 向你丢了一只随机皮卡丘(战斗力: 30530)!

--- all_test.go
+++ all_test.go
@@ -3,9 +3,7 @@ package engine_io
 import (
 	"fmt"
 
-	"sync"
 	"testing"
-	"time"
 )
 
 func TestPayload(t *testing.T) {
@@ -24,67 +22,4 @@ func TestPayload(t *testing.T) {
 	for _, p := range ps {
 		fmt.Printf("type=%d, data=%s\n", p.typo, p.data)
 	}
-
-}
-
-func TestChan(t *testing.T) {
-	ch := make(chan string, 10)
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		for {
-			var exit bool
-			select {
-			default:
-				exit = true
-				break
-			case s := <-ch:
-				fmt.Println("wahaha:", s)
-				break
-			}
-			if exit {
-				break
-			}
-		}
-		wg.Done()
-	}()
-	wg.Wait()
-}
-
-func TestChan2(t *testing.T) {
-	ch := make(chan uint32, 10)
-	go func() {
-		select {
-		default:
-			break
-		case <-ch:
-			break
-		}
-		fmt.Println("end")
-	}()
-
-	time.Sleep(time.Second * 10)
-
-}
-
-func TestChan3(t *testing.T) {
-	ch := make(chan uint32, 10)
-	go func() {
-		select {
-		default:
-			break
-		case <-ch:
-			break
-		}
-		fmt.Println("end")
-	}()
-
-	time.Sleep(time.Second * 10)
-
-}
-
-func Test4(t *testing.T) {
-	ch := make(chan *Packet, 10)
-	defer close(ch)
-	ch <- nil
 }
